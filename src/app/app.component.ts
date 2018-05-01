38f6ebd37f6c98c2981562a788a970d00246ed94
liupeng <1029538990@qq.com>
2018-05-01 20:18:33 +0800

    打印

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  resume = '';
+  text = `测试测
+  测试测
+  测试测
+  
+  测试测测试测测试测测试测
+  `;
+  temp: string[];
+  index = 0;
+
+  ngOnInit(): void {
+    this.temp = this.text.split('');
+    console.log(JSON.stringify(this.temp));
+    setInterval(()=>this.printing(), 300);
+  }
+
+  /**
+   *
+   */
+  private printing() {
+    this.resume += this.temp[this.index];
+    this.index++;
+  }
+
 }
